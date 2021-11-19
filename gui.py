449a9257523f674bf3ef05cd5268d41ee250d8a2
Bacon <85359439+AmberBacon@users.noreply.github.com>
2021-11-18 21:37:00 -0700

    Added POST method and cleaner HTML code

--- gui.py
+++ gui.py
@@ -4,10 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def my_form():
-    return render_template("index.html")
-
+    return render_template("test.html")
+    
 @app.route("/", methods=["POST"])
 def my_form_post():
+    #global tip
+    #global meal
+    #global tax
+    #global total 
     meal = float(request.form["meal"])
     tip = float(request.form["tip"])
     tax = float(request.form["tax"])
@@ -15,8 +19,8 @@ def my_form_post():
     total_tax = tax / 100
     meal = meal + (meal * total_tax)
     total = round((meal + (meal * total_tip)), 2)
-    return str(total)
-
+    return str("The total of your bill including tax and tip is " + str(total))
+        
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 
